623c57662599f63bc5ef3a1214bc5f61a4464445
Rajan Paudel <rajanpaudel6789@gmail.com>
2026-02-01 07:59:09 +0545

    Register Page

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,26 +1,31 @@
 function Register() {
   return (
-    <div>
-      <h1>Register</h1>
+    <div className="min-h-screen bg-gray-100 flex justify-center items-center">
+      
+      <form className="bg-blue-900 p-5 w-72 rounded">
+        <h1 className="text-xl text-center mb-4 text-white">
+          Register
+        </h1>
 
-      <form>
-        <label htmlFor="fname">First name:</label><br />
-        <input type="text" id="fname" name="fname" /><br />
+        <label className="text-white">First Name</label>
+        <input type="text" className="border w-full mb-3 p-1" />
 
-        <label htmlFor="lname">Last name:</label><br />
-        <input type="text" id="lname" name="lname" /><br />
+        <label className="text-white">Last Name</label>
+        <input type="text" className="border w-full mb-3 p-1" />
 
-        <label htmlFor="email">Email:</label><br />
-        <input type="email" id="email" name="email" /><br />
+        <label className="text-white">Email</label>
+        <input type="email" className="border w-full mb-3 p-1" />
 
-        <label htmlFor="password">Password:</label><br />
-        <input type="password" id="password" name="password" /><br />
-    
-           <button type="submit">Submit</button>
+        <label className="text-white">Password</label>
+        <input type="password" className="border w-full mb-4 p-1" />
+
+        <button className="bg-blue-500 text-white w-full p-2 rounded">
+          Submit
+        </button>
       </form>
+
     </div>
   );
 }
+
 export default Register;
-
-
